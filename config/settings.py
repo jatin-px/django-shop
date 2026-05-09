78609478e58b511b4d7490c5566eafbc8a2bfc89
Jatin P. <jatinpanchal.px@gmail.com>
2026-05-09 21:50:12 +0530

    ci: configure and fix GitHub Actions workflow

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config(
+    "SECRET_KEY",
+    default="django-insecure-dev-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config(
+    "DEBUG",
+    default=True,
+    cast=bool
+)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
@@ -80,7 +90,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / config("DATABASE_NAME"),
+        'NAME': BASE_DIR / config(
+            "DATABASE_NAME",
+            default="db.sqlite3"
+        ),
     }
 }
 
